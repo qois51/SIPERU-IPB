2728551129dc69dbb420dc20d51ee9e74c69c44b
Qois <qois.firosi@gmail.com>
2026-05-23 16:23:19 +0700

    Fix supaya bisa di push

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,10 @@
 from app.main import app
 import uvicorn
+import os
 
 if __name__ == "__main__":
-    uvicorn.run("app:app", host="127.0.0.1", port=8000, reload=True)
+    host = os.environ.get("HOST", "127.0.0.1")
+    port = int(os.environ.get("PORT", 8000))
+    if "PORT" in os.environ:
+        host = "0.0.0.0"
+    uvicorn.run("app:app", host=host, port=port, reload=("PORT" not in os.environ))
--- backend/run.py
+++ backend/run.py
@@ -1,4 +1,9 @@
 import uvicorn
+import os
 
 if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="127.0.0.1", port=8000, reload=True)
+    host = os.environ.get("HOST", "127.0.0.1")
+    port = int(os.environ.get("PORT", 8000))
+    if "PORT" in os.environ:
+        host = "0.0.0.0"
+    uvicorn.run("app.main:app", host=host, port=port, reload=("PORT" not in os.environ))
